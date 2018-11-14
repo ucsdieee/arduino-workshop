36d81cff3ff4f5cce42035cc51b1ba70b0cda28a
eminguyen <emilyhuongnguyen@gmail.com>
2018-11-14 13:40:10 -0800

    Add solution code

---
+++ SolutionCode/Arduino/ButtonLEDSolutions.ino
@@ -0,0 +1,28 @@
+/* File: ButtonLED.ino
+ * Turn on an LED by pressing a button
+ * Author: Antony Nguyen & INSERT YOUR NAME HERE!
+ */
+
+const int buttonPin = 7;
+const int ledPin = 9;
+int buttonState = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  if(buttonState == HIGH) {
+    // Turn the LED on
+
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    // Turn the LED off
+
+    digitalWrite(ledPin, LOW);
+
+  }
+}
